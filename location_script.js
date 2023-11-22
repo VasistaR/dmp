57b32128529756301fe954b4a3e742c4cb01fcd5
Vasista R <96642595+VasistaR@users.noreply.github.com>
2023-11-21 20:00:53 -0800

    Create location_script.js

---
+++ location_script.js
@@ -0,0 +1,26 @@
+  const options = {
+  enableHighAccuracy: true,
+  timeout: 1000,
+  maximumAge: 0,
+};
+
+var coord
+function success(pos) {
+  const crd = pos.coords;
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+   coord = [crd.longitude,crd.latitude];
+  console.log("map.center",coord);
+  return coord;
+
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+function getLocation(){
+ console.log("inside getLocation");
+  navigator.geolocation.getCurrentPosition(coord=success, error, options);
+}
